12ec12f58eb3153ac3affbb36c35ae9f490c390c
Luis Raul Espinoza Barboza - vm <respinozabarboza@gmail.com>
2017-08-16 11:51:11 -0600

    added test and view scripts

--- djaangorest/api/tests.py
+++ djaangorest/api/tests.py
@@ -4,6 +4,9 @@ from django.test import TestCase
 
 from django.test import TestCase
 from .models import Bucketlist
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
 
 class ModelTestCase(TestCase):
     """This class defines the test suite for the bucketlist model."""
@@ -12,6 +15,12 @@ class ModelTestCase(TestCase):
         """Define the test client and other test variables."""
         self.bucketlist_name = "Write world class code"
         self.bucketlist = Bucketlist(name=self.bucketlist_name)
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'Go to Ibiza'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format="json")
 
     def test_model_can_create_a_bucketlist(self):
         """Test the bucketlist model can create a bucketlist."""
@@ -19,3 +28,4 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
